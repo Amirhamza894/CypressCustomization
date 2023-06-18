b78b1989412e0b7d3314abba96547e35b9d45677
Amir Hamza Haq <50887466+Amirhamza894@users.noreply.github.com>
2023-06-18 17:24:44 +0500

    Update test.cy.js

--- cypress/e2e/horizontal/dubizzle-eg/test.cy.js
+++ cypress/e2e/horizontal/dubizzle-eg/test.cy.js
@@ -5,4 +5,10 @@ describe('testing cypress', function() {
         cy.url().should('be.equal', 'https://www.dubizzle.com.eg/en/')
 
     })
+
+    it('testing TC', () => {
+        cy.visit('https://www.dubizzle.com.eg/en/')
+        cy.url().should('be.equal', 'https://www.dubizzle.com.eg/en/')
+
+    })
 })
