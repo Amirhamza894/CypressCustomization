98257f1465c1ad6c8778d8366ab5a4295d885efa
amir <amir.hamza@empglabs.com>
2023-06-18 14:15:48 +0500

    Initially commit my dubizzle repo to github

---
+++ cypress/e2e/horizontal/dubizzle-eg/test.cy.js
@@ -0,0 +1,7 @@
+describe('testing cypress', function() {
+
+    it('testing TC', () => {
+        cy.visit('https://www.dubizzle.com.eg/en/')
+        cy.url().should('be.equal', 'https://www.dubizzle.com.eg/en/')
+    })
+})
