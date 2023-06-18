b2444c3b717f24fea0b414ff83f5e7db75ff1daf
amir <amir.hamza@empglabs.com>
2023-06-18 16:13:17 +0500

    edit circlici file

--- cypress/e2e/horizontal/dubizzle-eg/test.cy.js
+++ cypress/e2e/horizontal/dubizzle-eg/test.cy.js
@@ -3,5 +3,6 @@ describe('testing cypress', function() {
     it('testing TC', () => {
         cy.visit('https://www.dubizzle.com.eg/en/')
         cy.url().should('be.equal', 'https://www.dubizzle.com.eg/en/')
+        
     })
 })
