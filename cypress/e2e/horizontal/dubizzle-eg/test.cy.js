a9ea83da0db898771649bba71d726bd0bde134b0
Amir Hamza Haq <50887466+Amirhamza894@users.noreply.github.com>
2023-06-23 11:54:37 +0500

    Update test.cy.js

--- cypress/e2e/horizontal/dubizzle-eg/test.cy.js
+++ cypress/e2e/horizontal/dubizzle-eg/test.cy.js
@@ -2,7 +2,7 @@ describe('testing cypress', function() {
 
     it('testing TC', () => {
         const BASE_URL = Cypress.env('base_url');
-        cy.visit(BASE_URL)
+        cy.visit({ url: BASE_URL })
         cy.url().should('be.equal', BASE_URL)
 
     })
