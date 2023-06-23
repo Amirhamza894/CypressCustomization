40b2697815bfc7d1c5ce47d11aabe92c599c8eba
amir <amir.hamza@empglabs.com>
2023-06-23 15:38:12 +0500

    creating env variables in pipeline

--- cypress/e2e/horizontal/dubizzle-eg/test.cy.js
+++ cypress/e2e/horizontal/dubizzle-eg/test.cy.js
@@ -1,10 +1,9 @@
 describe('testing cypress', function() {
 
     it('testing TC', () => {
-        const BASE_URL = Cypress.env('base_url');
-        cy.visit(Cypress.env('base_url')
+        const BASE_URL = Cypress.env('base_url')
+        cy.visit(Cypress.env(BASE_URL))
         cy.url().should('be.equal', BASE_URL)
-
     })
 
 })
