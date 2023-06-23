add938ef0e6b85ca71bd6a3a51256c0662049571
amir <amir.hamza@empglabs.com>
2023-06-23 17:38:04 +0500

    updated the env variables

--- cypress/e2e/horizontal/dubizzle-eg/test.cy.js
+++ cypress/e2e/horizontal/dubizzle-eg/test.cy.js
@@ -1,9 +1,8 @@
 describe('testing cypress', function() {
 
     it('testing TC', () => {
-        const BASE_URL = Cypress.env('base_url');
-        cy.visit(BASE_URL)
-        cy.url().should('be.equal', BASE_URL)
+        cy.visit(Cypress.env('BASE_URL'))
+        cy.url().should('be.equal', Cypress.env('BASE_URL'))
     })
 
 })
