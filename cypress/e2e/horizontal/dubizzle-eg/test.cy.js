dddc99bf8d4aa33f8d0c43165d0817530223fcfd
haris <haris.fastian12@gmail.com>
2023-06-27 16:53:59 +0500

    added login case

--- cypress/e2e/horizontal/dubizzle-eg/test.cy.js
+++ cypress/e2e/horizontal/dubizzle-eg/test.cy.js
@@ -5,4 +5,15 @@ describe('testing cypress', function() {
         cy.url().should('be.equal', Cypress.env('BASE_URL'))
     })
 
+    it('should login', () => {
+        cy.visit(Cypress.env('BASE_URL'))
+        cy.get('._1b04dcc1 > ._261203a9').click()
+        cy.get('.c6ce5164._31a14546._42207ab4:nth-child(3)').click()
+        cy.get('#email').type(Cypress.env('EMAIL'))
+        cy.get('.a755fcd9 > button').click()
+        cy.get('#password').type(Cypress.env('PASSWORD'))
+        cy.get('.a755fcd9 > button > span').click()
+
+    })
+
 })
