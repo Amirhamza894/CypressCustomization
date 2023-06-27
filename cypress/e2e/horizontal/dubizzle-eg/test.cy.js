01fffbba71ed26a20438461a2ad1f2a2b2099d3e
haris <haris.fastian12@gmail.com>
2023-06-27 17:13:21 +0500

    fixed login case

--- cypress/e2e/horizontal/dubizzle-eg/test.cy.js
+++ cypress/e2e/horizontal/dubizzle-eg/test.cy.js
@@ -1,18 +1,23 @@
 describe('testing cypress', function() {
 
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false;
+      })
+
     it('testing TC', () => {
         cy.visit(Cypress.env('BASE_URL'))
         cy.url().should('be.equal', Cypress.env('BASE_URL'))
     })
 
-    it('should login', () => {
+    it.only('should login', () => {
         cy.visit(Cypress.env('BASE_URL'))
         cy.get('._1b04dcc1 > ._261203a9').click()
-        cy.get('.c6ce5164._31a14546._42207ab4:nth-child(3)').click()
+        cy.get('._21b291bd > :nth-child(3)').click()
         cy.get('#email').type(Cypress.env('EMAIL'))
         cy.get('.a755fcd9 > button').click()
+        .wait(5000)
         cy.get('#password').type(Cypress.env('PASSWORD'))
-        cy.get('.a755fcd9 > button > span').click()
+        cy.get('.a755fcd9 > ._5fd7b300').click()
 
     })
 
